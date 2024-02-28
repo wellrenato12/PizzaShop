10c12871e0956e0dad349627d80fbf97ef024461
Wellington Albino <well.renato@hotmail.com>
2024-02-28 20:46:06 -0300

    Continuando testes

---
+++ src/api/mocks/index.ts
@@ -0,0 +1,31 @@
+import { setupWorker } from 'msw/browser'
+
+import { env } from '@/env'
+
+import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
+import { getDayOrdersAmountMock } from './get-day-orders-amount'
+import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount'
+import { getMonthOrdersAmountMock } from './get-month-orders-amount'
+import { getMonthRevenueMock } from './get-month-revenue'
+import { getPoularProductsMock } from './get-popular-products-mock'
+import { registerRestaurantMock } from './register-restaurant-mock'
+import { signInMock } from './sign-in-mock'
+
+export const worker = setupWorker(
+  signInMock,
+  registerRestaurantMock,
+  getDayOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthCanceledOrdersAmountMock,
+  getMonthRevenueMock,
+  getDailyRevenueInPeriodMock,
+  getPoularProductsMock,
+)
+
+export async function enableMSW() {
+  if (env.MODE !== 'test') {
+    return
+  }
+
+  await worker.start()
+}
